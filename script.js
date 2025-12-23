6ea0081b56ba036d325cbf961fe3477298f5f24e
LokiVN92 <127079658+LokiVN92@users.noreply.github.com>
2025-12-23 15:40:31 +0700

    script.js

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ const SHEET_URL =
 /* ===== RANDOM CÓ TỶ LỆ ===== */
 function weightedRandom(min, max) {
   const split = 2000000;
-  if (Math.random() < 0.98) {
+  if (Math.random() < 0.5) {
     const t = Math.random() ** 2.5;
     return Math.floor(min + t * (Math.min(split, max) - min));
   }
